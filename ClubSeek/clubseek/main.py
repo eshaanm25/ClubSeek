c69027e5f1c1a863b7cf0a579137779f7f48bdd8
Eshaan Mathur <eshaanm25@gmail.com>
2022-04-04 00:41:43 -0400

    feat: Added Basic Database Integration

--- ClubSeek/clubseek/main.py
+++ ClubSeek/clubseek/main.py
@@ -1,7 +1,29 @@
 from multiprocessing.connection import wait
 from flask import Flask, request, jsonify
+import mysql.connector
+from mysql.connector import Error
 import time
 
+
+def create_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host='clubdatabases',
+            user=user_name,
+            passwd=user_password,
+            db = 'clubdatabase'
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred. Attempting to reconnect")
+        time.sleep(5)
+
+    return connection
+
+connection = None
+while connection == None:
+    connection = create_connection("127.0.0.1", "user", "password")
 
 # Initialize Application
 app = Flask(__name__)
